4eb4120095592592c2fee5e04cedb860bf0f7adb
Jumala9163 <weareh0711@gmail.com>
2022-08-17 03:08:55 +0900

    1.1.27-komelab-beta.1

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -3,9 +3,20 @@ module.exports = {
     {
       header: 'BetaRelease.1',
       content: 'とりあえず日本語化したギリ',
-      advanced: { cm: '' }
+      advanced: { cm: 'f64a692' }
+    },
+    {
+      header: 'BetaRelease.1',
+      content: 'ロゴをKome-Lab仕様に変更したギリ(仮)',
+      advanced: { cm: 'dd4b093' }
     }
   ],
-  improvements: [],
+  improvements: [
+    {
+      header: 'BetaRelease.1',
+      content: 'wip',
+      advanced: { cm: '9a8e143' }
+    }
+  ],
   bugfixes: []
 };
