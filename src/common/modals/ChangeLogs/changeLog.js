b6212e0618a96bc497b8e8b5f232c226ae9f68e6
Jumala9163 <weareh0711@gmail.com>
2022-08-19 23:09:41 +0900

    v1.1.27-komelab-beta.3

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,44 +1,18 @@
 module.exports = {
-  new: [
-    {
-      header: 'BetaRelease.1',
-      content: 'とりあえず日本語化したギリ',
-      advanced: { cm: 'f64a692' }
-    },
-    {
-      header: 'BetaRelease.1',
-      content: 'ロゴをKome-Lab仕様に変更したギリ(仮)',
-      advanced: { cm: 'dd4b093' }
-    },
-    {
-      header: 'BetaRelease.2',
-      content: 'RemoveNews',
-      advanced: { cm: '99dfaa' }
-    }
-  ],
+  new: [],
   improvements: [
     {
-      header: 'BetaRelease.1',
-      content: 'wip',
-      advanced: { cm: '9a8e143' }
-    },
-    {
-      header: 'BetaRelease.2',
-      content:
-        '日本語の修正・uiの微調整・その他細かい修正・画像の差し替えをしたギリ',
-      advanced: { cm: '1200f4' }
+      header: 'BetaRelease.3',
+      content: '日本語の微調整',
+      advanced: { cm: 'a1d797' }
     }
   ],
   bugfixes: [
     {
-      header: 'BetaRelease.2-2',
-      content: 'fix icon.png size',
-      advanced: { cm: '4bbc50' }
-    },
-    {
-      header: 'BetaRelease.2-3',
-      content: 'fix icon size',
-      advanced: { cm: '1ca8fa' }
+      header: 'BetaRelease.3',
+      content:
+        '一般設定のリリースチャンネルの設定が正常に実行されない問題を修正したギリ',
+      advanced: { cm: 'a1d797' }
     }
   ]
 };
