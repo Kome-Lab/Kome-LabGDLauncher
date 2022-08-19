9437d898feb68990d07d2ffaa95a30db70251ec8
Jumala9163 <weareh0711@gmail.com>
2022-08-20 00:58:02 +0900

    v1.1.27-komelab-beta.4

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -2,17 +2,10 @@ module.exports = {
   new: [],
   improvements: [
     {
-      header: 'BetaRelease.3',
-      content: '日本語の微調整',
-      advanced: { cm: 'a1d797' }
+      header: 'BetaRelease.4',
+      content: 'uiと日本語の微調整',
+      advanced: { cm: '64ee92' }
     }
   ],
-  bugfixes: [
-    {
-      header: 'BetaRelease.3',
-      content:
-        '一般設定のリリースチャンネルの設定が正常に実行されない問題を修正したギリ',
-      advanced: { cm: 'a1d797' }
-    }
-  ]
+  bugfixes: []
 };
