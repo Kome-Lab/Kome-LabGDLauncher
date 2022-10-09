63c3298fd3305ab2b89f564af02fb1640e155638
Jumala9163 <weareh0711@gmail.com>
2022-10-10 01:43:04 +0900

    v1.1.29-kome-lab.2

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,42 +1,24 @@
 module.exports = {
-  new: [],
-  improvements: [
+  new: [
     {
-      header: '1.1.29-komelab-beta.1',
-      content: '日本語の修正及び軽微な修正',
-      advanced: { cm: '4290df4' }
+      header:
+        'インスタンス設定からシェーダーパックの管理ができるようになりました！！',
+      content:
+        'OptiFineとかを使っている人は今まで以上にシェーダーパックの管理がしやすくなりました',
+      advanced: { cm: '55410b' }
     }
   ],
-  bugfixes: [
+  improvements: [
     {
-      header: '1.1.29-komelab-beta.1',
-      content: 'fix code',
-      advanced: { cm: '074a1b7' }
+      header: 'update banner logo and linux-icons and logo',
+      content: 'ロゴの一部を修正',
+      advanced: { cm: '69b169' }
     },
     {
-      header: 'No longer killing the game',
-      content: 'when closing the Launcher.',
-      advanced: { cm: '391dd9cc', pr: '1412' }
-    },
-    {
-      header: 'Fixed the game resolution setting',
-      content: 'being ignored.',
-      advanced: { cm: '87f89ed9', pr: '1429' }
-    },
-    {
-      header: 'Fixed the kill button',
-      content: 'having to be clicked twice.',
-      advanced: { cm: '2664f8bb', pr: '1419' }
-    },
-    {
-      header: 'Fixed error code one ',
-      content: 'when certain java args are missing.',
-      advanced: { cm: 'cdae501a', pr: '1420' }
-    },
-    {
-      header: 'Fixed settings resetting',
-      content: 'caused by the CurseForge workaround.',
-      advanced: { cm: '7f29ca64', pr: '1431' }
+      header: 'インスタンス設定、スクリーンショットの項目の日本語を修正',
+      content: '',
+      advanced: { cm: '4296cf' }
     }
-  ]
+  ],
+  bugfixes: []
 };
