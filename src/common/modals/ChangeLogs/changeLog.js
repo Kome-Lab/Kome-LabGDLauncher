7e818e8cbda51f040d1af8916d139e9197a877e7
Jumala9163 <weareh0711@gmail.com>
2022-10-10 02:35:53 +0900

    v1.1.29-kome-lab.3

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,24 +1,11 @@
 module.exports = {
-  new: [
+  new: [],
+  improvements: [],
+  bugfixes: [
     {
-      header:
-        'インスタンス設定からシェーダーパックの管理ができるようになりました！！',
-      content:
-        'OptiFineとかを使っている人は今まで以上にシェーダーパックの管理がしやすくなりました',
-      advanced: { cm: '55410b' }
-    }
-  ],
-  improvements: [
-    {
-      header: 'update banner logo and linux-icons and logo',
-      content: 'ロゴの一部を修正',
-      advanced: { cm: '69b169' }
-    },
-    {
-      header: 'インスタンス設定、スクリーンショットの項目の日本語を修正',
+      header: 'スクリーンショットのプレビューが正常に表示されない問題を修正',
       content: '',
-      advanced: { cm: '4296cf' }
+      advanced: { cm: '4b2058' }
     }
-  ],
-  bugfixes: []
+  ]
 };
