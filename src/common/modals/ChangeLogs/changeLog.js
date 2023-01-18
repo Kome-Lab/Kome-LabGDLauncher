e9f70ceab23af85afec485b28be589907dac4d32
Jumala9163 <weareh0711@gmail.com>
2023-01-19 00:30:30 +0900

    v1.1.29-kome-lab.4

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,11 +1,35 @@
 module.exports = {
   new: [],
-  improvements: [],
+  improvements: [
+    {
+      header:
+        'Move nsfw to dependencies, remove napi & murmur2 binaries. (gorilla-devs#1446)',
+      content: '',
+      advanced: { cm: '20148d6' }
+    },
+    {
+      header: 'Updated base URL for fetching news images (gorilla-devs#1443)',
+      content: '',
+      advanced: { cm: 'efa324a' }
+    },
+    {
+      header: 'Allow for Local ARM64 Linux Building (gorilla-devs#1451)',
+      content: '',
+      advanced: { cm: '4fd9a47' }
+    },
+    {
+      header:
+        'Fix java arch being inconsistent with the manifest (gorilla-devs#1515)',
+      content: '',
+      advanced: { cm: '2f20cd9' }
+    }
+  ],
   bugfixes: [
     {
-      header: 'スクリーンショットのプレビューが正常に表示されない問題を修正',
+      header:
+        'Update microsoft URI from HTTP to HTTPS - gorilla-devs#1513 (gorilla-devs#1514)',
       content: '',
-      advanced: { cm: '4b2058' }
+      advanced: { cm: '73b3f48' }
     }
   ]
 };
