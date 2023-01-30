5612f0be41fc723766051db9165774ed6dd2e418
Jumala9163 <weareh0711@gmail.com>
2023-01-30 14:34:55 +0900

    v1.1.30-komelab1-beta.1

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,32 +1,5 @@
 module.exports = {
   new: [],
-  improvements: [
-    {
-      header:
-        'Fix broken windows nsfw build introduced in gorilla-devs#1446 (gorilla-devs#1518)',
-      content: '',
-      advanced: { cm: 'a8dfa1c' }
-    },
-    {
-      header: 'Add architecture-dependend metadata for ARM (gorilla-devs#1521)',
-      content: '',
-      advanced: { cm: 'e69a7e3' }
-    },
-    {
-      header: 'ensure user path exists',
-      content: '',
-      advanced: { cm: '36f9548' }
-    },
-    {
-      header: 'lint fix',
-      content: '',
-      advanced: { cm: '7fg15c7' }
-    },
-    {
-      header: 'fix',
-      content: '',
-      advanced: { cm: '671a76' }
-    }
-  ],
+  improvements: [],
   bugfixes: []
 };
