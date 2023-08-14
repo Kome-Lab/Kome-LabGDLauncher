e113bd3dae382f49b0d544116b56a3b2702e7e2f
Jumala9163 <weareh0711@gmail.com>
2023-08-14 21:29:38 +0900

    v1.1.30-kome-lab.2

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,64 +1,11 @@
 module.exports = {
   new: [
     {
-      header: 'Support ARM',
-      content: 'Architecture.( gorilla-devs#1451 )',
-      advanced: { cm: '4fd9a4' }
-    },
-    {
-      header: 'Add manual download',
-      content: 'option for failed opted out mods.( gorilla-devs#1512 )',
-      advanced: { cm: 'a8dfa1' }
+      header: 'Remove FTB on request of them.',
+      content: 'Also breaking API changes were done after the documentation was taken down. Rolling back this commit won’t help.( gorilla-devs#1569 )',
+      advanced: { cm: '97c33ac' }
     }
   ],
-  improvements: [
-    {
-      header: 'Updated url',
-      content: 'for minecraft news images.( gorilla-devs#1443 )',
-      advanced: { cm: 'efa324' }
-    },
-    {
-      header: 'Simplifications',
-      content:
-        ' to the codebase, napi and nsfw now get automatically compiled on build.( gorilla-devs#1446 )',
-      advanced: { cm: '20148d' }
-    },
-    {
-      header: 'Add restore option',
-      content: 'to failed updates.( gorilla-devs#1531 )',
-      advanced: { cm: '9d84085' }
-    },
-    {
-      header: 'update path',
-      content: '',
-      advanced: { cm: 'ef5326' }
-    },
-    {
-      header: 'update Japanece',
-      content: '',
-      advanced: { cm: '4c4d58' }
-    },
-    {
-      header: 'fix url and name',
-      content: '',
-      advanced: { cm: '650d02' }
-    },
-    {
-      header: 'update max downloads value',
-      content: '',
-      advanced: { cm: 'e7a0f0' }
-    }
-  ],
-  bugfixes: [
-    {
-      header: 'Fix asset downloading',
-      content: 'now enforcing https.( gorilla-devs#1514 )',
-      advanced: { cm: '73b3f4' }
-    },
-    {
-      header: 'Fix deprecated warnings',
-      content: 'for dropped file handles.( gorilla-devs#1531 )',
-      advanced: { cm: '9d84085' }
-    }
-  ]
+  improvements: [],
+  bugfixes: []
 };
