f86c51353cfe7078aa9a8e09d106c28a6658716d
Jumala9163 <weareh0711@gmail.com>
2023-08-14 21:50:45 +0900

    v1.1.30-kome-lab.2

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -2,7 +2,8 @@ module.exports = {
   new: [
     {
       header: 'Remove FTB on request of them.',
-      content: 'Also breaking API changes were done after the documentation was taken down. Rolling back this commit won’t help.( gorilla-devs#1569 )',
+      content:
+        'Also breaking API changes were done after the documentation was taken down. Rolling back this commit wont help.( gorilla-devs#1569 )',
       advanced: { cm: '97c33ac' }
     }
   ],
